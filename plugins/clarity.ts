9ffc0c08141acd663b78e47b89d346b243d2be27
veryCrunchy <verycrunchydev@gmail.com>
2024-02-21 00:36:49 +0100

    feat: clarity

---
+++ plugins/clarity.ts
@@ -0,0 +1,14 @@
+export default defineNuxtPlugin(() => {
+  addRouteMiddleware(
+    "clarity",
+    () => {
+      if (process.client) {
+        const script = document.createElement("script");
+        script.type = "text/javascript";
+        script.innerHTML = `(function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window, document, "clarity", "script", "l4yfvq08qj");`;
+        document.head.appendChild(script);
+      }
+    },
+    { global: true }
+  );
+});
